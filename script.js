60cdff724d04aeb794328bec9c4bac40cbdcac55
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-09 15:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timerDisplay = document.getElementById("timer");
+let current = new Date();
+
+ setInterval( 
+	 function(){
+		current = new Date();
+		timerDisplay.innerHTML = current.toLocaleString();
+	 }  , 1000);
